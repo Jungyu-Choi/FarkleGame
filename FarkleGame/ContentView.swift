e8bc720851faf5712473f673ab5fa4953f38bb56
Jungyu Choi <zmfjscl789@gmail.com>
2020-08-08 17:34:19 +0900

    Initial Commit

---
+++ FarkleGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FarkleGame
+//
+//  Created by Jungyu Choi on 2020/08/08.
+//  Copyright © 2020 Jungyu Choi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
